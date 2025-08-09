a02d7220784fd7b0d34932737cff1e50aec88de9
alxzy <alandikasaputra989@gmail.com>
2025-08-09 16:53:42 +0700

    Update ubuntu22.04.sh

--- Install/ubuntu22.04.sh
+++ Install/ubuntu22.04.sh
@@ -1,125 +1,104 @@
 #!/data/data/com.termux/files/usr/bin/bash
-pkg install root-repo x11-repo
-pkg install proot xz-utils pulseaudio -y
+set -e
+
+# === Persiapan awal ===
+pkg install -y root-repo x11-repo proot xz-utils pulseaudio wget tar
 termux-setup-storage
-##Code Name Ubuntu (Core)
-# - trusty (Ubuntu 14.04)  ||  - bionic (Ubuntu 18.04)
-# - xenial (Ubuntu 16.04)  ||  - focal  (Ubuntu 20.04)
-ubuntu=jammy
-folder=ubuntu-fs
-if [ -d "$folder" ]; then
-        first=1
-        echo "Skipping Downloading"
+
+# === Konfigurasi ===
+UBUNTU_VERSION="jammy"  # ganti sesuai kebutuhan
+FOLDER="ubuntu-fs"
+TARBALL="ubuntu-rootfs.tar.gz"
+BIN_SCRIPT=".ubuntu"
+LAUNCH_CMD="ubuntu"
+
+# === Deteksi apakah rootfs sudah ada ===
+if [ -d "$FOLDER" ]; then
+    echo "[INFO] Folder $FOLDER sudah ada, skip download."
+    FIRST=1
 fi
-tarball="ubuntu-rootfs.tar.gz"
-if [ "$first" != 1 ];then
-        if [ ! -f $tarball ]; then
-                echo "Download Rootfs, this may take a while base on your internet speed."
-                case `dpkg --print-architecture` in
-                aarch64)
-                        archurl="arm64" ;;
-                arm*)
-                        archurl="armhf" ;;
-                ppc64el)
-                        archurl="ppc64el" ;;
-                x86_64)
-                        archurl="amd64" ;;
-                *)
-                        echo "Unknown Architecture"; exit 1 ;;
-                esac
-                wget "https://partner-images.canonical.com/core/${ubuntu}/current/ubuntu-${ubuntu}-core-cloudimg-${archurl}-root.tar.gz" -O $tarball
-                #wget "https://partner-images.canonical.com/oci/${ubuntu}/current/ubuntu-${ubuntu}-oci-${archurl}-root.tar.gz" -O $tarball
-        fi
-        cur=`pwd`
-        mkdir -p "$folder"
-        cd "$folder"
-        echo "Decompressing Rootfs, please be patient."
-        proot --link2symlink tar -xf ${cur}/${tarball}||:
-        cd "$cur"
-   fi
-   echo "ubuntu" > ~/"$folder"/etc/hostname
-   echo "127.0.0.1 localhost" > ~/"$folder"/etc/hosts
-   echo "nameserver 8.8.8.8" > ~/"$folder"/etc/resolv.conf
-mkdir -p $folder/binds
-bin=.ubuntu
-linux=ubuntu
-echo "writing launch script"
-cat > $bin <<- EOM
+
+# === Download rootfs jika belum ada ===
+if [ "$FIRST" != 1 ]; then
+    if [ ! -f "$TARBALL" ]; then
+        echo "[INFO] Mengunduh Ubuntu Rootfs..."
+        case $(dpkg --print-architecture) in
+            aarch64) ARCH_URL="arm64" ;;
+            arm*) ARCH_URL="armhf" ;;
+            ppc64el) ARCH_URL="ppc64el" ;;
+            x86_64) ARCH_URL="amd64" ;;
+            *) echo "[ERROR] Arsitektur tidak dikenal!"; exit 1 ;;
+        esac
+        wget "https://partner-images.canonical.com/core/${UBUNTU_VERSION}/current/ubuntu-${UBUNTU_VERSION}-core-cloudimg-${ARCH_URL}-root.tar.gz" -O "$TARBALL"
+    fi
+
+    mkdir -p "$FOLDER"
+    echo "[INFO] Mengekstrak Rootfs..."
+    proot --link2symlink tar -xf "$TARBALL" -C "$FOLDER" || true
+fi
+
+# === Setting awal sistem ===
+echo "ubuntu" > "$FOLDER/etc/hostname"
+echo "127.0.0.1 localhost" > "$FOLDER/etc/hosts"
+echo "nameserver 8.8.8.8" > "$FOLDER/etc/resolv.conf"
+mkdir -p "$FOLDER/binds"
+
+# === Perbaikan error group name ===
+cat > "$FOLDER/usr/sbin/groupadd" <<'EOF'
+#!/bin/sh
+exit 0
+EOF
+chmod +x "$FOLDER/usr/sbin/groupadd"
+
+cat > "$FOLDER/usr/sbin/useradd" <<'EOF'
+#!/bin/sh
+exit 0
+EOF
+chmod +x "$FOLDER/usr/sbin/useradd"
+
+# === Buat script launch ===
+cat > "$BIN_SCRIPT" <<- EOM
 #!/bin/bash
 pulseaudio --start \
     --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" \
-    --exit-idle-time=-1
-cd \$(dirname \$0)
-## Unset LD_PRELOAD in case termux-exec is installed
+    --exit-idle-time=-1 >/dev/null 2>&1
+
 unset LD_PRELOAD
-command="proot"
-command+=" --kill-on-exit"
-command+=" --link2symlink"
-command+=" -0"
-command+=" -r $folder"
-if [ -n "\$(ls -A $folder/binds)" ]; then
-    for f in $folder/binds/* ;do
-      . \$f
-    done
+command="proot --kill-on-exit --link2symlink -0 -r $FOLDER"
+if [ -n "\$(ls -A $FOLDER/binds 2>/dev/null)" ]; then
+    for f in $FOLDER/binds/*; do . "\$f"; done
 fi
 command+=" -b /dev"
 command+=" -b /dev/null:/proc/sys/kernel/cap_last_cap"
 command+=" -b /proc"
 command+=" -b /data/data/com.termux/files/usr/tmp:/tmp"
-command+=" -b $folder/root:/dev/shm"
-## Uncomment the following line to have access to the home directory of termux
-#command+=" -b /data/data/com.termux/files/home:/root"
-## Uncomment the following line to mount /sdcard directly to /
+command+=" -b $FOLDER/root:/dev/shm"
 command+=" -b /sdcard"
 command+=" -w /root"
-command+=" /usr/bin/env -i"
-command+=" HOME=/root"
-command+=" PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games"
-command+=" TERM=\$TERM"
-command+=" LANG=C.UTF-8"
-command+=" /bin/bash --login"
-com="\$@"
-if [ -z "\$1" ];then
+command+=" /usr/bin/env -i HOME=/root PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin TERM=\$TERM LANG=C.UTF-8 /bin/bash --login"
+
+if [ -z "\$1" ]; then
     exec \$command
 else
-    \$command -c "\$com"
+    \$command -c "\$*"
 fi
 EOM
-   #Fixing shebang of $linux"
-   termux-fix-shebang $bin
-   #Making $linux executable"
-   chmod +x $bin
-   #Removing image for some space"
-   #rm $tarball
-#Repositories
-#echo "#Ubuntu Repositories
-#deb http://ports.ubuntu.com/ubuntu-ports jammy main restricted universe multiverse
-#deb http://ports.ubuntu.com/ubuntu-ports jammy-updates main restricted universe multiverse
-#deb http://ports.ubuntu.com/ubuntu-ports jammy-security main restricted universe multiverse
-#deb http://ports.ubuntu.com/ubuntu-ports jammy-proposed main restricted universe multiverse
-#deb http://ports.ubuntu.com/ubuntu-ports jammy-backports main restricted universe multiverse" > ~/"$folder"/etc/apt/sources.list
-echo "export PULSE_SERVER=127.0.0.1" >> $folder/etc/skel/.bashrc
-echo '#!/bin/bash
-bash .ubuntu' > $PREFIX/bin/$linux
-chmod +x $PREFIX/bin/$linux
-   clear
-   echo ""
-   echo "Updating Ubuntu,.."
-   echo ""
-echo "#!/bin/bash
-touch ~/.hushlogin
-apt update && apt upgrade -y
-apt install apt-utils dialog nano -y
-cp /etc/skel/.bashrc .
-rm -rf ~/.bash_profile
-exit" > $folder/root/.bash_profile
-bash $linux
-   clear
-   echo ""
-   echo "You can login to Ubuntu with 'ubuntu' script next time"
-   echo ""
-   #rm ubuntu22.04.sh
 
-#
-## Script edited by 'WaHaSa', Script V3-revision.
-#
+termux-fix-shebang "$BIN_SCRIPT"
+chmod +x "$BIN_SCRIPT"
+
+# === Shortcut di Termux ===
+echo "#!/bin/bash
+bash $BIN_SCRIPT" > "$PREFIX/bin/$LAUNCH_CMD"
+chmod +x "$PREFIX/bin/$LAUNCH_CMD"
+
+# === Setup default Ubuntu ===
+echo 'export PULSE_SERVER=127.0.0.1' >> "$FOLDER/etc/skel/.bashrc"
+echo "touch ~/.hushlogin
+apt update && apt upgrade -y
+apt install -y apt-utils dialog nano
+cp /etc/skel/.bashrc ~
+rm -f ~/.bash_profile" > "$FOLDER/root/.bash_profile"
+
+clear
+echo "[INFO] Instalasi selesai. Ketik '$LAUNCH_CMD' untuk masuk Ubuntu."
